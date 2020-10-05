9cc88ae8c1088e3acf5923819535bf38b5965ea0
BaptisteMahe <mahe.baptiste.19@gmail.com>
2020-10-05 19:22:58 +0200

    ZXP-1 Import MatCardModule

--- front-angular/src/app/modules/material.module.ts
+++ front-angular/src/app/modules/material.module.ts
@@ -14,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
 
 
 import { SelectAutocompleteModule } from 'mat-select-autocomplete';
@@ -34,7 +35,8 @@ import { SelectAutocompleteModule } from 'mat-select-autocomplete';
     MatDialogModule,
     MatButtonModule,
     MatIconModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
