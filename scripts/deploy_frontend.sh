613e998ca1c895ada754d13f826b2a3727a54185
BaptisteMahe <mahe.baptiste.19@gmail.com>
2020-11-08 15:24:02 +0100

    Faster Angular Build

--- scripts/deploy_frontend.sh
+++ scripts/deploy_frontend.sh
@@ -3,7 +3,7 @@
 ssh -t persil@ovh1.ec-m.fr rm -rf www/zero-xp
 
 npm install
-ng build --prod --base-href ./
+ng build --prod --source-map=false --base-href ./
 
 tar -czvf zero-xp.tar.gz dist/zero-xp
 scp zero-xp.tar.gz persil@ovh1.ec-m.fr:
