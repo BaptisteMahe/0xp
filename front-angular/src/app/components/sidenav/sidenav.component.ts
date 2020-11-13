9e82deb503402567a1909789607c7612bfc58201
Marceau Clavel <clavelmarceau@gmail.com>
2020-11-13 16:14:00 +0100

    added conditionnal display on sidnav menu links

--- front-angular/src/app/components/sidenav/sidenav.component.ts
+++ front-angular/src/app/components/sidenav/sidenav.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
 import { Observable } from 'rxjs';
 
+import { UserService} from '../../services';
+import { User } from 'src/models';
+
 @Component({
   selector: 'app-sidenav',
   templateUrl: 'sidenav.component.html',
@@ -9,15 +12,21 @@ import { Observable } from 'rxjs';
 })
 export class SidenavComponent implements OnInit{
 
+  currentUser: User;
+
   @Input()
   toggleSidenavObs: Observable<void>;
 
   @ViewChild('drawer')
   drawer: MatDrawer;
 
-  constructor(){ }
+  constructor(private userService: UserService){ }
 
   ngOnInit(){
+    this.userService.getCurrentUserObs().subscribe((user: User) => {
+      this.currentUser = user;
+    });
+
     this.toggleSidenavObs.subscribe(() => {
       this.drawer.toggle();
     });
