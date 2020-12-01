3e44169e624f0b1610fa5dd9abeae5b57a18bec1
Baptiste Mahé <mahe.baptiste.19@gmail.com>
2020-12-01 19:51:53 +0100

    Update deploy_frontend_marceau.sh

--- scripts/deploy_frontend_marceau.sh
+++ scripts/deploy_frontend_marceau.sh
@@ -10,7 +10,7 @@ tar -czvf zero-xp.tar.gz dist/zero-xp
 scp zero-xp.tar.gz github@marceauclavel.com:
 rm -rf dist zero-xp.tar.gz
 
-ssh -t  tar -xzvf zero-xp.tar.gz
+ssh -t github@marceauclavel.com tar -xzvf zero-xp.tar.gz
 ssh -t github@marceauclavel.com cp -r dist/zero-xp/* /var/www/front-0xp/
 ssh -t github@marceauclavel.com rm -rf zero-xp.tar.gz dist
 
