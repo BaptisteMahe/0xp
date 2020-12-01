872eb10830ed2fd33cacb338b152943a7adf285a
Marceau Clavel <clavelmarceau@gmail.com>
2020-12-01 19:17:48 +0100

    fix script for deployment marceauclavel.com

--- scripts/deploy_frontend_marceau.sh
+++ scripts/deploy_frontend_marceau.sh
@@ -1,7 +1,7 @@
 
 #!/usr/bin bash
 
-ssh -t github@marceauclavel.com rm -rf /var/www/0xp-front-angular
+ssh -t github@marceauclavel.com rm -rf /var/www/front-0xp/*
 
 npm install
 ng build --prod --source-map=false --base-href ./
@@ -10,9 +10,8 @@ tar -czvf zero-xp.tar.gz dist/zero-xp
 scp zero-xp.tar.gz github@marceauclavel.com:
 rm -rf dist zero-xp.tar.gz
 
-ssh -t github@marceauclavel.com tar -xzvf zero-xp.tar.gz
-ssh -t github@marceauclavel.com cp -r dist/zero-xp/ www/0xp-front-angular
+ssh -t  tar -xzvf zero-xp.tar.gz
+ssh -t github@marceauclavel.com cp -r dist/zero-xp/* /var/www/front-0xp/
 ssh -t github@marceauclavel.com rm -rf zero-xp.tar.gz dist
-ssh -t github@marceauclavel.com cp -r www/zero-xp/assets/* www/0xp-front-angular/assets/
 
 exit 0
