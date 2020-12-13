38f627d0f35abe288f832679ac099d902be12956
Marceau Clavel <clavelmarceau@gmail.com>
2020-12-13 12:50:36 +0100

    refactor AvisController

--- back-node/controllers/AvisController.js
+++ back-node/controllers/AvisController.js
@@ -4,30 +4,29 @@ let bodyParser = require('body-parser');
 router.use(bodyParser.json());
 let ObjectId = require('mongodb').ObjectId
 
-router.get('/', function(req, res, next) {
-    db.collection('avis').find().toArray(function(err, results){
-        res.json(results)
-    })
+router.get('/', function(req, res) {
+    db.collection('avis').find().toArray()
+        .then(result => res.json(result))
+        .catch(err => {res.send(err)});
 });
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function(req, res) {
     const avidObjectId = new ObjectId(req.params.id)
     db.collection('avis').findOne({_id : avidObjectId})
         .then(company => company ? res.json(company) : res.sendStatus(404))
-        .catch(err => next(err));
+        .catch(err => {res.send(err)});
 });
 
-router.get('/company/:id', function(req, res, next) {
-    db.collection('avis').find({idCompany : req.params.id}).toArray(function(err, results){
-        res.json(results)
-    })
+router.get('/company/:id', function(req, res) {
+    db.collection('avis').find({idCompany : req.params.id}).toArray()
+        .then(result => res.json(result))
+        .catch(err => {res.send(err)});
 });
 
-router.post('/', function(req, res, next) {    
-    db.collection('avis').insertOne(req.body).then(() => res.json({}))
-            .catch(err => next(err));
+router.post('/', function(req, res) {    
+    db.collection('avis').insertOne(req.body)
+        .then(() => res.json({}))
+        .catch(err => {res.send(err)});
 });
-
-
 
 module.exports = router;
