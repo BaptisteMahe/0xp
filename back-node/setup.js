2004dacd1ace7c093797474d7dc96d0350170e26
BaptisteMahe <mahe.baptiste.19@gmail.com>
2020-12-19 16:17:47 +0100

    ZXP-168 Add setup.js but not working

--- back-node/setup.js
+++ back-node/setup.js
@@ -5,28 +5,28 @@ const MongoClient = require('mongodb').MongoClient;
 
 //CORS Middleware
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
 });
 
 //listen
 const uri = "mongodb+srv://admin0xp:mdpadmin0xp@0xp-aqxy3.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
-    useNewUrlParser: true
+  useNewUrlParser: true
 });
 client.connect(err => {
-    if (err) return console.log(err);
-    db = client.db('0xpDB');
-    setup(db)
-    console.log("done")
+  if (err) return console.log(err);
+  db = client.db('0xpDB');
+  setup(db)
+  console.log("done")
 });
 
 let setup = (db) => {
-    // TODO enter all the setup to generate the mongodb instance
-    // create collections
-    // apply validators
-    db.collection('companies').createIndex({"name": 1},{unique: true})
-    db.collection('users').createIndex({"username": 1},{unique: true})
+  // TODO enter all the setup to generate the mongodb instance
+  // create collections
+  // apply validators
+  db.collection('companies').createIndex({ "name": 1 }, { unique: true })
+  db.collection('users').createIndex({ "username": 1 }, { unique: true })
 }
