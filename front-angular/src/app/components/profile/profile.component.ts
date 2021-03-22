9b422f8dd1e5d94b14bb37cd6e25e28058a19f7c
BaptisteMahe <mahe.baptiste.19@gmail.com>
2021-03-22 23:26:33 +0100

    Add offer-link to home page for companies

--- front-angular/src/app/components/profile/profile.component.ts
+++ front-angular/src/app/components/profile/profile.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { UserService, AuthenticationService } from '../../services';
 import { User } from '../../../models';
+
 
 @Component({
   selector: 'app-profile',
@@ -11,15 +14,29 @@ import { User } from '../../../models';
 })
 export class ProfileComponent implements OnInit {
 
+  state$: Observable<any>;
+
   currentUser: User;
+
+  tabIndex: number;
 
   constructor(private userService: UserService,
               private authenticationService: AuthenticationService,
-              private router: Router) { }
+              private router: Router,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.userService.getCurrentUserObs().subscribe((user: User) => {
       this.currentUser = user;
+    });
+
+    this.state$ = this.activatedRoute.paramMap
+        .pipe(map(() => window.history.state));
+
+    this.state$.subscribe(data => {
+      if (data?.tab === 'offer') {
+        this.tabIndex = 1;
+      }
     });
   }
 
